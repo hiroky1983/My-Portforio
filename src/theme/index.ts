e8a35136af1e306d357d8c78720cd5df26977f04
yamadahiroki <hirockysan1983@gmail.com>
2021-10-15 23:00:11 +0900

    💄 Chakraのグローバルスタイル設定

---
+++ src/theme/index.ts
@@ -0,0 +1,15 @@
+import { extendTheme, theme } from "@chakra-ui/react";
+
+export const costomTheme = extendTheme({
+  ...theme,
+  styles: {
+    global: {
+      "html, body": {
+        margin: 0,
+        FontFace: {
+          fontFamily: "Noto Sans KR",
+        },
+      },
+    },
+  },
+});
