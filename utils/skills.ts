a3011e69d4282327afe09dc79d909259e7c2b390
yamadahiroki <hirockysan1983@gmail.com>
2021-12-18 12:11:51 +0900

    ✨ スキルセットのTableを実装

--- utils/skills.ts
+++ utils/skills.ts
@@ -1,14 +1,65 @@
 export const skillsTable = [
   { id: 1, name: "HTML", learn: "", duration: "1年" },
-  { id: 2, name: "CSS", learn: "", duration: "1年" },
-  { id: 3, name: "JavaScript", learn: "", duration: "11ヶ月" },
-  { id: 4, name: "React", learn: "", duration: "11ヶ月" },
-  { id: 5, name: "Next.js", learn: "", duration: "9ヶ月" },
-  { id: 6, name: "TypeScript", learn: "", duration: "8ヶ月" },
-  { id: 7, name: "Firebase", learn: "", duration: "8ヶ月" },
-  { id: 8, name: "tailwindcss", learn: "", duration: "9ヶ月" },
-  { id: 9, name: "Git,GitHub", learn: "", duration: "11ヶ月" },
-  { id: 10, name: "Jira", learn: "", duration: "4ヶ月" },
+  {
+    id: 2,
+    name: "CSS",
+    learn:
+      "基礎のclass,idなどのセレクターでの記法はだいたい書けます。今後CSSアニメーションなど学習していきたいです。",
+    duration: "1年",
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    learn:
+      "基礎的な記法はOK、学習初めてすぐReactを始めた為、DOM操作のメソッドなどは調べながら実装になります。",
+    duration: "11ヶ月",
+  },
+  {
+    id: 4,
+    name: "React",
+    learn: "React hooks,props,など基本的な部分は一通り学習出来たかと思います。",
+    duration: "11ヶ月",
+  },
+  {
+    id: 5,
+    name: "Next.js",
+    learn:
+      "基本的なLinkやImage、ファイルシステムルーティングなど、個人を使う上で必ず一緒に使用しています。SSRやSSGなど今後キャッチアップ予定です。",
+    duration: "9ヶ月",
+  },
+  {
+    id: 6,
+    name: "TypeScript",
+    learn:
+      "まだ学習中ですが、typeやinterfaceなどの型付けやprops、VFCやFCの型付などは経験しています。",
+    duration: "8ヶ月",
+  },
+  {
+    id: 7,
+    name: "Firebase",
+    learn:
+      "主にFirestoreを学習して来ました。v9の記法も簡単なものはキャッチアップ済み、rulesの記述などの理解もある程度は出来ています",
+    duration: "8ヶ月",
+  },
+  {
+    id: 8,
+    name: "tailwindcss",
+    learn: "基礎的な記法は一通り出来ます。JITモードなどの設定を今後学習予定",
+    duration: "9ヶ月",
+  },
+  {
+    id: 9,
+    name: "Git,GitHub",
+    learn:
+      "Github連携、チーム開発でブランチを切ったりプルリク作成なども経験してます。",
+    duration: "11ヶ月",
+  },
+  {
+    id: 10,
+    name: "Jira",
+    learn: "チーム開発でのTask管理に使用",
+    duration: "4ヶ月",
+  },
 ];
 
 export const bietNewSkills = [
