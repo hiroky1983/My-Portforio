78f5a8400837020d0c83a28f88dc4dde5753589c
yamadahiroki <hirockysan1983@gmail.com>
2022-01-06 23:14:06 +0900

    ✨ Skillの経験年数を変数で動的に変更出来るようにした

--- utils/skills.ts
+++ utils/skills.ts
@@ -1,64 +1,71 @@
+import { experienceTime } from "./today";
+
 export const skillsTable = [
-  { id: 1, name: "HTML", learn: "タグの理解、属性の理解", duration: "1年" },
+  {
+    id: 1,
+    name: "HTML",
+    learn: "タグの理解、属性の理解",
+    duration: experienceTime(2020, 11),
+  },
   {
     id: 2,
     name: "CSS",
     learn:
       "基礎のclass,idなどのセレクターでの記法はだいたい書けます。今後CSSアニメーションなど学習していきたいです。",
-    duration: "1年",
+    duration: experienceTime(2020, 11),
   },
   {
     id: 3,
     name: "JavaScript",
     learn:
       "基礎的な記法はOK、学習初めてすぐReactを始めた為、DOM操作のメソッドなどは調べながら実装になります。",
-    duration: "11ヶ月",
+    duration: experienceTime(2020, 12),
   },
   {
     id: 4,
     name: "React",
     learn: "React hooks,props,など基本的な部分は一通り学習出来たかと思います。",
-    duration: "11ヶ月",
+    duration: experienceTime(2021, 2),
   },
   {
     id: 5,
     name: "Next.js",
     learn:
       "基本的なLinkやImage、ファイルシステムルーティングなど、個人を使う上で必ず一緒に使用しています。SSRやSSGなど今後キャッチアップ予定です。",
-    duration: "9ヶ月",
+    duration: experienceTime(2021, 3),
   },
   {
     id: 6,
     name: "TypeScript",
     learn:
       "まだ学習中ですが、typeやinterfaceなどの型付けやprops、VFCやFCの型付などは経験しています。",
-    duration: "8ヶ月",
+    duration: experienceTime(2021, 6),
   },
   {
     id: 7,
     name: "Firebase",
     learn:
       "主にFirestoreを学習して来ました。v9の記法も簡単なものはキャッチアップ済み、rulesの記述などの理解もある程度は出来ています",
-    duration: "8ヶ月",
+    duration: experienceTime(2021, 5),
   },
   {
     id: 8,
     name: "tailwindcss",
     learn: "基礎的な記法は一通り出来ます。JITモードなどの設定を今後学習予定",
-    duration: "9ヶ月",
+    duration: experienceTime(2021, 3),
   },
   {
     id: 9,
     name: "Git,GitHub",
     learn:
       "Github連携、チーム開発でブランチを切ったりプルリク作成なども経験してます。",
-    duration: "11ヶ月",
+    duration: experienceTime(2021, 1),
   },
   {
     id: 10,
     name: "Jira",
     learn: "チーム開発でのTask管理に使用",
-    duration: "4ヶ月",
+    duration: experienceTime(2021, 8),
   },
 ];
 
@@ -68,6 +75,7 @@ export const bietNewSkills = [
   { id: 3, name: "TypeScript" },
   { id: 4, name: "Firebase" },
   { id: 5, name: "Chakra UI" },
+  { id: 6, name: "tailwindcss" },
 ];
 
 export const memberChoosedSkills = [
--- utils/today.ts
+++ utils/today.ts
@@ -6,3 +6,19 @@ export const getToday = () => {
   const YMD = `${year}年${month}月${date}日`;
   return YMD;
 };
+
+export const experienceTime = (startYear: number, startMonth: number) => {
+  const nowDate = new Date();
+  const startDate = new Date(startYear, startMonth - 1);
+
+  const diff = nowDate.getTime() - startDate.getTime();
+  const getDate = diff / (1000 * 60 * 60 * 24);
+  const DAYS_PER_MONTH = 365 / 12;
+  const expYear = Math.floor(getDate / 365);
+  const expMonth = Math.floor((getDate - 365 * expYear) / DAYS_PER_MONTH);
+
+  const expTime =
+    expYear === 0 ? `${expMonth}ヶ月` : `${expYear}年${expMonth}ヶ月`;
+
+  return expTime;
+};
