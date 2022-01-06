b3133c0c1ee84fd6c94dcbf56f0569e67047420a
yamadahiroki <hirockysan1983@gmail.com>
2022-01-06 23:11:26 +0900

    🎨 Table幅の調整

--- src/container/Skilltable.tsx
+++ src/container/Skilltable.tsx
@@ -4,20 +4,20 @@ import { skillsTable } from "../../utils/skills";
 
 export const SkillTable = () => {
   return (
-    <Table variant="simple">
+    <Table>
       <Thead>
         <Tr>
-          <Th>スキル</Th>
-          <Th textAlign="center">学習内容</Th>
-          <Th>期間</Th>
+          <Th w="20%">スキル</Th>
+          <Th textAlign="center" w="60%">学習内容</Th>
+          <Th w="20%">期間</Th>
         </Tr>
       </Thead>
       {skillsTable.map((skill) => (
         <Tbody key={skill.id}>
           <Tr>
-            <Td>{skill.name}</Td>
-            <Td>{skill.learn}</Td>
-            <Td>{skill.duration}</Td>
+            <Td w="20%">{skill.name}</Td>
+            <Td w="60%">{skill.learn}</Td>
+            <Td w="20%">{skill.duration}</Td>
           </Tr>
         </Tbody>
       ))}
